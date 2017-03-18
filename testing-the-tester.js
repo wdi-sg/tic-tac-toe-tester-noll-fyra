f45339b9aa50418e99c3492ff206271f3e9a2dfa
Jonathan Ng <jonathanlouisng@gmail.com>
2017-03-18 23:23:48 +0800

    refactor the tester

--- testing-the-tester.js
+++ testing-the-tester.js
@@ -1,65 +1,73 @@
 var movesTaken = []
-var playerState = [0, 1, 2, 3, 4, 5, 6, 7, 8]
+var grid = [0, 1, 2, 3, 4, 5, 6, 7, 8]
 var playerOneActive = true
-var draw = false
+var isDraw = false
 
+// reset the game variables
 function restart () {
   movesTaken = []
-  playerState = [0, 1, 2, 3, 4, 5, 6, 7, 8]
+  grid = [0, 1, 2, 3, 4, 5, 6, 7, 8]
   playerOneActive = true
-  draw = false
+  isDraw = false
 }
 
+// checks if a winning combination is met or the last piece is placed
 function isGameOver () {
-  if (movesTaken.length === 0) {
-    return false
-  } else if (movesTaken.length >= 5) {
-    switch (true) {
-      case (playerState[0] === playerState[1] && playerState[1] === playerState[2]):
-        return true
-      case (playerState[3] === playerState[4] && playerState[4] === playerState[5]):
-        return true
-      case (playerState[6] === playerState[7] && playerState[7] === playerState[8]):
-        return true
-      case (playerState[0] === playerState[3] && playerState[3] === playerState[6]):
-        return true
-      case (playerState[1] === playerState[4] && playerState[4] === playerState[7]):
-        return true
-      case (playerState[2] === playerState[5] && playerState[5] === playerState[8]):
-        return true
-      case (playerState[0] === playerState[4] && playerState[4] === playerState[8]):
-        return true
-      case (playerState[2] === playerState[4] && playerState[4] === playerState[6]):
-        return true
-      case (movesTaken.length === 9):
-        draw = true
-        break
-      default:
-        return false
-    }
-  } else {
-    return false
+  switch (true) {
+    // top row
+    case (grid[0] === grid[1] && grid[1] === grid[2]):
+      return true
+    // middle row
+    case (grid[3] === grid[4] && grid[4] === grid[5]):
+      return true
+    // bottom row
+    case (grid[6] === grid[7] && grid[7] === grid[8]):
+      return true
+    // left column
+    case (grid[0] === grid[3] && grid[3] === grid[6]):
+      return true
+    // middle column
+    case (grid[1] === grid[4] && grid[4] === grid[7]):
+      return true
+    // right column
+    case (grid[2] === grid[5] && grid[5] === grid[8]):
+      return true
+    // diagonal \
+    case (grid[0] === grid[4] && grid[4] === grid[8]):
+      return true
+    // diagonal /
+    case (grid[2] === grid[4] && grid[4] === grid[6]):
+      return true
+    // draw
+    case (movesTaken.length === 9):
+      isDraw = true
+      break
+    default:
+      return false
   }
 }
 
+// if isGameOver() it returns the winner or draw
 function whoWon () {
-  if (isGameOver() && playerOneActive === true) {
-    return 1
-  } else if (isGameOver() && playerOneActive === false) {
-    return 2
-  } else if (draw === true) {
-    return 3
-  } else {
-    return 0
+  switch (true) {
+    case (isGameOver() && playerOneActive):
+      return 1
+    case (isGameOver() && !playerOneActive):
+      return 2
+    case (isDraw):
+      return 3
+    default:
+      return 0
   }
 }
 
+// checks if the move is valid and changes the active player
 function playTurn (index) {
   if (movesTaken.includes(index)) {
     return false
   } else {
     movesTaken.push(index)
-    playerState[index] = playerOneActive
+    grid[index] = playerOneActive
     if (!isGameOver()) {
       playerOneActive = !playerOneActive
     }
